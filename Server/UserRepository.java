f85b7466e858ffb1d2be8ba981b2a929c68dc609
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-03-13 23:44:00 +0900

    Update UserRepository.java

--- Server/UserRepository.java
+++ Server/UserRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUserid(String userid);
-    //userid가 이미 데이터베이스에 존재하는지
     boolean existsByUserid(String userid);
 }
