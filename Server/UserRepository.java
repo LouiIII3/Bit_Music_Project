b2ca4c0f349939d4b4e20c5d6f5722dafb29c22f
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-03-24 13:28:39 +0900

    Delete Server/UserRepository.java

--- Server/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.social.media;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUserid(String userid);
-    boolean existsByUserid(String userid);
-    
-}
