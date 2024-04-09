3788b861a1f01e60d6f8674894e69d44afb87a3c
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-04-09 19:20:02 +0900

    Update JoinDTO.java

--- Server/music/v2/dto/JoinDTO.java
+++ Server/music/v2/dto/JoinDTO.java
@@ -6,9 +6,9 @@ import lombok.Setter;
 @Setter
 @Getter
 public class JoinDTO {
+    
     private String username;
     private String password;
-
     private String nickname;
     private String profilePictureUrl;
 }
