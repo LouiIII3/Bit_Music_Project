bac464ab030d8127d45ae1712a133ceb117cb7dd
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-06-15 18:09:13 +0900

    Create App.vue

---
+++ Web/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <mv_net />
+  </div>
+</template>
+
+<script>
+import mv_net from './components/main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    mv_net,
+  },
+};
+
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
